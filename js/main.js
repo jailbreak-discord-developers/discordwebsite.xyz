cd06c8f2023fbc0578a79aa37f8106ad7312831a
Turnt Ducky <turntducky@gmail.com>
2020-04-05 22:28:02 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,142 +9,189 @@
     }
   })
 
-      /*-------------------------------------------------------------------------------
+  /*-------------------------------------------------------------------------------
 
-        Smooth scrolling
+    Smooth scrolling
 
-      -------------------------------------------------------------------------------*/
+  -------------------------------------------------------------------------------*/
 
-      $(document).ready(function() {
-        $("a").on('click', function(event) {
+  $(document).ready(function() {
+    $("a").on('click', function(event) {
 
-          if (this.hash !== "") {
-            event.preventDefault();
+      if (this.hash !== "") {
+        event.preventDefault();
 
-            var hash = this.hash;
+        var hash = this.hash;
 
-            $('html, body').animate({
-              scrollTop: $(hash).offset().top
-            }, 800, function() {
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function() {
 
-              window.location.hash = hash;
-            });
-          }
+          window.location.hash = hash;
         });
-      });
+      }
+    });
+  });
 
-      /*-------------------------------------------------------------------------------
+  /*-------------------------------------------------------------------------------
 
 	  Navbar
 
 	-------------------------------------------------------------------------------*/
 
-        $('a.js-scroll[href*="#"]:not([href="#"])').on("click", function() {
-          if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-              $('html, body').animate({
-                scrollTop: (target.offset().top - navHeight + 5)
-              }, 1000, "easeInOutExpo");
-              return false;
-            }
-          }
-        });
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
 
-        $('.js-scroll').on("click", function() {
-          $('.navbar-collapse').collapse('hide');
-        });
+  /*--/ Star ScrollTop /--*/
+  $('.scrolltop-mf').on("click", function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1000);
+  });
 
-        $('body').scrollspy({
-          target: '#mainNav',
-          offset: navHeight
-        });
+  /*--/ Star Counter /--*/
+  $('.counter').counterUp({
+    delay: 15,
+    time: 2000
+  });
 
-        $(window).trigger('scroll');
-        $(window).on('scroll', function() {
-          var pixels = 50;
-          var top = 1200;
-          if ($(window).scrollTop() > pixels) {
-            $('.navbar-expand-md').addClass('navbar-reduce');
-            $('.navbar-expand-md').removeClass('navbar-trans');
-          } else {
-            $('.navbar-expand-md').addClass('navbar-trans');
-            $('.navbar-expand-md').removeClass('navbar-reduce');
-          }
-          if ($(window).scrollTop() > top) {
-            $('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
-          } else {
-            $('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
-          }
-        });
+  /*--/ Star Scrolling nav /--*/
+  $('a.js-scroll[href*="#"]:not([href="#"])').on("click", function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - navHeight + 5)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
 
-        /*-------------------------------------------------------------------------------
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll').on("click", function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: navHeight
+  });
+  /*--/ End Scrolling nav /--*/
+
+  /*--/ Navbar Menu Reduce /--*/
+  $(window).trigger('scroll');
+  $(window).on('scroll', function() {
+    var pixels = 50;
+    var top = 1200;
+    if ($(window).scrollTop() > pixels) {
+      $('.navbar-expand-md').addClass('navbar-reduce');
+      $('.navbar-expand-md').removeClass('navbar-trans');
+    } else {
+      $('.navbar-expand-md').addClass('navbar-trans');
+      $('.navbar-expand-md').removeClass('navbar-reduce');
+    }
+    if ($(window).scrollTop() > top) {
+      $('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
+    } else {
+      $('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
+    }
+  });
+
+  /*--/ Star Typed /--*/
+  if ($('.text-slider').length == 1) {
+    var typed_strings = $('.text-slider-items').text();
+    var typed = new Typed('.text-slider', {
+      strings: typed_strings.split(','),
+      typeSpeed: 80,
+      loop: true,
+      backDelay: 1100,
+      backSpeed: 30
+    });
+  }
+
+})(jQuery);
+
+/*-------------------------------------------------------------------------------
 
 	  clients logo slider
 
 	-------------------------------------------------------------------------------*/
 
-        if ($('.clients_slider').length) {
-          $('.clients_slider').owlCarousel({
-            loop: true,
-            margin: 30,
-            items: 5,
-            nav: false,
-            dots: false,
-            responsiveClass: true,
-            autoplay: 2500,
-            slideSpeed: 300,
-            paginationSpeed: 500,
-            responsive: {
-              0: {
-                items: 1,
-              },
-              768: {
-                items: 3,
-              },
-              1024: {
-                items: 4,
-              },
-              1224: {
-                items: 5
-              }
-            }
-          })
-        }
+if ($('.clients_slider').length) {
+  $('.clients_slider').owlCarousel({
+    loop: true,
+    margin: 30,
+    items: 5,
+    nav: false,
+    dots: false,
+    responsiveClass: true,
+    autoplay: 2500,
+    slideSpeed: 300,
+    paginationSpeed: 500,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 3,
+      },
+      1024: {
+        items: 4,
+      },
+      1224: {
+        items: 5
+      }
+    }
+  })
+}
 
-        /*-------------------------------------------------------------------------------
+/*-------------------------------------------------------------------------------
 
 	  testimonial slider
 
 	-------------------------------------------------------------------------------*/
 
-        if ($('.testimonial').length) {
-          $('.testimonial').owlCarousel({
-            loop: true,
-            margin: 30,
-            items: 5,
-            nav: false,
-            dots: true,
-            responsiveClass: true,
-            slideSpeed: 300,
-            paginationSpeed: 500,
-            responsive: {
-              0: {
-                items: 1
-              }
-            }
-          })
-        }
+if ($('.testimonial').length) {
+  $('.testimonial').owlCarousel({
+    loop: true,
+    margin: 30,
+    items: 5,
+    nav: false,
+    dots: true,
+    responsiveClass: true,
+    slideSpeed: 300,
+    paginationSpeed: 500,
+    responsive: {
+      0: {
+        items: 1
+      }
+    }
+  })
+}
 
-        /*-------------------------------------------------------------------------------
+/*-------------------------------------------------------------------------------
 
 	  Mailchimp
 
 	-------------------------------------------------------------------------------*/
 
-        function mailChimp() {
-          $('#mc_embed_signup').find('form').ajaxChimp();
-        }
-        mailChimp();
-      })(jQuery);
+function mailChimp() {
+  $('#mc_embed_signup').find('form').ajaxChimp();
+}
+mailChimp();
+})(jQuery);
